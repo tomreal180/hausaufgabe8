9ff2b7b40665bd437a985b0af5c3db77df77b455
MinhDucHa <ducha180102@gmail.com>
2022-11-29 14:49:52 +0100

    Attribut und Konstruktor von Position hinzufügen

--- Position.java
+++ Position.java
@@ -1,5 +1,19 @@
 package hausaufgabe8;
 
-public class Position {
+import java.util.ArrayList;
 
+public class Position {
+		private ArrayList<Integer> pos;
+		public Position(int x,int y) throws WrongPositionException {
+			if(x<1||x>8||y<1||y>8) {
+				throw new WrongPositionException("die Position liegt außerhalb des Bretts");
+			}else {
+				ArrayList<Integer> list= new ArrayList<>();
+				list.add(x);
+				list.add(y);
+				this.pos=list;
+			}
+			
+		}
+		
 }
